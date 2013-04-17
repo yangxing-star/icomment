15ce28bc840c220045a73ee84fcf028c75c88fcc
star <xing.yang@taodinet.com>
2013-04-17 20:48:00 +0800

    init porject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icomment::Application.config.secret_token = '003beacf81bdc204417cd2247415a1404b44f139da1de88b7f5ee5a7544704dd2d3c0c6a9515229974580a5211d78f2cb7a6096774088b2f579714bdaf058bc2'
